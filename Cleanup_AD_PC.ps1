ec951f358d041e6e91490d270d0555345d5082ab
Ryan Adams <uncletygg@yahoo.com>
2025-04-07 08:48:59 -0400

    Add files via upload

---
+++ Cleanup_AD_PC.ps1
@@ -0,0 +1,6 @@
+﻿# Finds computers that haven't been logged into in a year, then pings each of those machines once and outputs the result to a text file. #
+$datecutoff = (Get-Date).AddDays(365)
+Get-ADComputer -Properties LastLogonDate -Filter {LastLogonDate -ge $datecutoff} | FT Name >Computers.txt
+Pause
+Test-Connection -computername (Get-Content Computers.txt) >C:\pinged.txt -Count 1
+Pause
